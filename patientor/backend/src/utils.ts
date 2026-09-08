fe344402663f230961c3d3613b3a25c2048f313b
chengunkier <inabamicheal97@gmail.com>
2026-09-08 14:23:36 +0300

    completed exercise 14. Patientor backend, step6

--- patientor/backend/src/utils.ts
+++ patientor/backend/src/utils.ts
@@ -1,5 +1,5 @@
-import type { NewPatientEntry, Gender } from './types.ts';
-import { Gender as GenderValues } from './types.ts';
+import { Gender } from './types.ts';
+import type { NewPatientEntry } from './types.ts';
 
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
@@ -10,39 +10,39 @@ const isDate = (date: string): boolean => {
 };
 
 const isGender = (param: string): param is Gender => {
-  return Object.values(GenderValues as unknown as Record<string, string>).includes(param);
+  return Object.values(Gender as Record<string, string>).includes(param);
 };
 
 const parseName = (name: unknown): string => {
-  if (!name || !isString(name)) {
+  if (!isString(name)) {
     throw new Error('Incorrect or missing name');
   }
   return name;
 };
 
 const parseDateOfBirth = (dateOfBirth: unknown): string => {
-  if (!dateOfBirth || !isString(dateOfBirth) || !isDate(dateOfBirth)) {
+  if (!isString(dateOfBirth) || !isDate(dateOfBirth)) {
     throw new Error('Incorrect or missing dateOfBirth: ' + dateOfBirth);
   }
   return dateOfBirth;
 };
 
 const parseSsn = (ssn: unknown): string => {
-  if (!ssn || !isString(ssn)) {
+  if (!isString(ssn)) {
     throw new Error('Incorrect or missing ssn');
   }
   return ssn;
 };
 
 const parseGender = (gender: unknown): Gender => {
-  if (!gender || !isString(gender) || !isGender(gender)) {
+  if (!isString(gender) || !isGender(gender)) {
     throw new Error('Incorrect or missing gender: ' + gender);
   }
   return gender;
 };
 
 const parseOccupation = (occupation: unknown): string => {
-  if (!occupation || !isString(occupation)) {
+  if (!isString(occupation)) {
     throw new Error('Incorrect or missing occupation');
   }
   return occupation;
