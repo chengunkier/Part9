b122a5c063a7cf165fe050d9e84e70e84ea6f0b2
chengunkier <inabamicheal97@gmail.com>
2026-09-15 23:11:07 +0300

    completed exrcise 25. Patientor, step 3

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -2,10 +2,7 @@ import { v1 as uuid } from 'uuid';
 import patientsData from '../../data/patients.ts';
 import type { NonSensitivePatient, NewPatientEntry, Patient } from '../types.ts';
 
-const patients: Patient[] = (patientsData as Omit<Patient, 'entries'>[]).map((patient) => ({
-  ...patient,
-  entries: []
-}));
+const patients: Patient[] = patientsData as Patient[];
 
 const getNonSensitiveEntries = (): NonSensitivePatient[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -14,9 +14,56 @@ export const Gender = {
 
 export type Gender = typeof Gender[keyof typeof Gender];
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+
+export type HealthCheckRating = typeof HealthCheckRating[keyof typeof HealthCheckRating];
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export const NewPatientSchema = z.object({
   name: z.string(),
